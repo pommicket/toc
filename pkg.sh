29e8e65be7b07515b3f45a67593602d9e4317115
Leo Tenenbaum <pommicket@gmail.com>
2020-01-16 20:37:04 -0500

    deleted unnecessary files

--- pkg.sh
+++ pkg.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 std_things="io arr"
 cd std
+
+VALGRIND="valgrind --track-origins=yes --exit-on-first-error=yes --error-exitcode=1 -q"
+if [ "$1" = "nov" ]; then
+	VALGRIND=""
+fi
+if [ "$CC" = "" ]; then
+	CC=gcc
+fi
+
 for thing in $std_things; do
-	valgrind --track-origins=yes --exit-on-first-error=yes --error-exitcode=1 -q ../toc $thing.toc -o $thing.c || exit 1
+	$VALGRIND ../toc $thing.toc -o $thing.c || exit 1
 done
 cd ..
-valgrind --track-origins=yes --exit-on-first-error=yes --error-exitcode=1 -q ./toc test.toc || exit 1
-gcc out.c std/*.c || exit 1
+$VALGRIND ./toc test.toc || exit 1
+$CC out.c std/*.c || exit 1
 ./a.out
