ea45f46f883c6192319f200290c55544db7b53e1
Leo Tenenbaum <pommicket@gmail.com>
2020-05-05 13:47:42 -0400

    improved testing

--- test-all.sh
+++ test-all.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 ./test-build.sh || exit 1
 tests/test.sh || exit 1
+./build.sh release || exit 1
+tests/test.sh || exit 1
--- test-build.sh
+++ test-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "tcc -Wall main.c -o toc" && tcc -Wall main.c -o toc || exit 1 # test without any compiler flags
+echo "tcc -DCOMPILE_TIME_FOREIGN_FN_SUPPORT=0 -Wall main.c -o toc" && tcc -DCOMPILE_TIME_FOREIGN_FN_SUPPORT=0 -Wall main.c -o toc || exit 1 # test without compile time foreign fn support
 for CC in tcc gcc clang g++ ; do
 	CC="$CC" CFLAGS='-Werror' ./build.sh || exit 1
 	CC="$CC" CFLAGS='-Werror' ./build.sh release || exit 1
